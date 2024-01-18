c6e41c2f3033b940d31dc8ef2d200677eb2d2457
Caio Theodoro de Carvalho <caio.carvalho@coxautoinc.com>
2024-01-17 22:48:21 -0300

    initial exploration

---
+++ VotingSystem.Test/CounterTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace VotingSystem.Test;
+
+public class CounterTests
+{
+    [Fact]
+    public void CounterHasName(){
+        var name = "Counter Name";
+        var counter = new Counter();
+        
+        Assert.Equal(name, counter.Name);
+    }
+
+}
+
+public class Counter
+{
+    public string Name { get; set; }
+    public Counter()
+    {
+    }
+}
